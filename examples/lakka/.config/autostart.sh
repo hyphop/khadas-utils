ffe60e16faed649bb16b50a7f05041cde5abbcac
hyphop <email2tema@gmail.com>
2019-09-21 12:23:36 +0300

    Sat Sep 21 12:23:36 MSK 2019

--- examples/lakka/.config/autostart.sh
+++ examples/lakka/.config/autostart.sh
@@ -6,7 +6,23 @@ LOG="/tmp/autostart.log"
 
 echo "[i] started $(date)">> $LOG
 
-# mkdir ~/opt/
+# // install
+#
+# create this script ~/.config/autostart.sh
+#
+# // requires
+#
+# mkdir ~/opt && cd ~/opt
+#
+# // get static busybox https://github.com/hyphop/khadas-busybox/releases/tag/1.0
+#
+# wget https://github.com/hyphop/khadas-busybox/releases/download/1.0/busybox
+#
+# // get khadas fan
+#
+# wget https://raw.githubusercontent.com/hyphop/khadas-utils/master/utils/fan
+#
+# // welldone 
 
-~/opt/fan daemon
+sh ~/opt/fan daemon
 
